07a72eb0820eefe58700f38ef3dfbe6146e89e43
Ahmad Hamdan <ahmadmhdhamdan@gmail.com>
2026-04-25 00:03:43 -0400

    Amend guessword section size and font on mobile

--- src/Letter.js
+++ src/Letter.js
@@ -1,7 +1,7 @@
 const Letter = ({ letter, showLetter, revealRed, revealHint }) => {
   return (
     <>
-      <div className="text-center w-12 h-12 last:mr-0 mr-4 pb-2.5 font-primary text-4xl border-b-4 border-b-black">
+      <div className="text-center w-7 h-7 md:w-12 md:h-12 last:mr-0 mr-2 md:mr-4 pb-1 md:pb-2.5 font-primary text-xl md:text-4xl border-b-2 md:border-b-4 border-b-black">
         {showLetter ? (
           revealHint ? (
             <span className="text-yellow-400">{letter}</span>
